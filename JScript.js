4857fd3779a8aa9b718102fa3641492425c645d8
Selectmedrip <arman.papiants@yandex.ru>
2024-10-12 17:37:33 +0300

    v1

---
+++ JScript.js
@@ -0,0 +1,6 @@
+document.getElementById("P1").onclick = function () {
+    window.location.href = "wa.me/+79165176990";
+};
+document.getElementById("P2").onclick = function () {
+    window.location.href = "wa.me/+79850107548";
+};
